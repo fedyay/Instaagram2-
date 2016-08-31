eb12a850a232818b5a75999a1ad81cb3699a4312
fedyay <danil.df.df@gmail.com>
2016-08-31 17:46:09 +0600

    Adding devise gem for users.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < Devise::RegistrationsController
+  def root
+  	
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true, length: {maximum: 50}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :friends, :class_name => 'User'
-	belongs_to :user
+	belongs_to :user, optional: true
 end
