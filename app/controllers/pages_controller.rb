b4ddcbd4e537a2a8157b8087d2418e2ae22c9dd8
fedyay <danil.df.df@gmail.com>
2016-09-04 18:56:29 +0600

    Added ordering images in feed and profile.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
 class PagesController < ApplicationController
+  before_action :block
   def root
     @pics = []
     current_user.friends.each do |f|
-      @pics << f.images.to_a 
+      @pics << f.images
     end
-    @pics = @pics.to_a
+    @pics[0] = @pics[0].order(id: :desc)
   end
 
   def show
-  	@images = current_user.images
+  	@images = current_user.images.order(id: :desc)
   end
 
   def friendship
@@ -22,5 +23,14 @@ class PagesController < ApplicationController
     @img = Image.find(params[:format])
     @img.liked_by current_user
     redirect_to root_path
+  end
+
+  private
+
+  def block
+    if current_user == nil
+      flash[:danger] = "Please log in"
+      redirect_to new_user_session_path
+    end
   end
 end
